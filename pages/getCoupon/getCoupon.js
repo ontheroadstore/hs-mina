84f3d26e3fd490c99acda12c188151d08d16fdbf
szp <szh1871@163.com>
2018-09-03 19:22:17 +0800

    [优惠券]领取优惠券

--- pages/getCoupon/getCoupon.js
+++ pages/getCoupon/getCoupon.js
@@ -40,8 +40,11 @@ Page({
     req(app.globalData.bastUrl, 'appv6/coupon/getUserCouponList',{}, 'GET').then(res => {
       if(res.status==1){
         //todo:套数据 & 领取功能
+        res.data.seller.forEach(item => {
+          item.coupon = this.formatCouponData(item.coupon)
+        })
         this.setData({
-          allCoupon:res.data.all,
+          allCoupon:this.formatCouponData(res.data.all),
           sellerCoupon: res.data.seller,
         })
       }else{
@@ -50,4 +53,58 @@ Page({
     })
 
   },
+  formatCouponData: function (data) {
+    if (data && data.length > 0) {
+      data.forEach((v, i, arr) => {
+        if (v.min_price > 0) {
+          v.coupon_tip = '满' + v.min_price + '可用';
+        } else {
+          v.coupon_tip = '消费任意金额可用';
+        }
+        if (v.apply_time_type == 2) {
+          let startTime = (new Date()).getTime();
+          let endTime = startTime + v.apply_time_length * 24 * 60 * 60 * 1000;
+          v.start_time = this.couponFmtTime(startTime);
+          v.end_time = this.couponFmtTime(endTime);
+        } else {
+          v.start_time = this.couponFmtTime(v.apply_time_start);  // 使用开始时间
+          v.end_time = this.couponFmtTime(v.apply_time_end);       // 使用结束时间
+        }
+        v.can_get = 1; //是否可领
+      })
+      return data;
+    } else {
+      return [];
+    }
+  },
+  couponFmtTime: function (time) {
+    function fixNum(v) {
+      return v < 10 ? '0' + v : v;
+    }
+    time = String(time).length === 10 ? time * 1000 : time;
+    var t = new Date(time);
+    var y = fixNum(t.getFullYear());
+    var m = fixNum(t.getMonth() + 1);
+    var d = fixNum(t.getDate());
+    return y + '.' + m + '.' + d;
+  },
+  //领取优惠券
+  receiveCoupon(e){
+    let couponId = e.target.dataset.couponid;
+    req(app.globalData.bastUrl, `appv6/coupon/${couponId}/receive`, {}, 'POST').then(res => {
+      if(res.status == 1){
+        wx.showToast({
+          title: '领取成功',
+          icon: 'success',
+          duration: 2000
+        })
+      }else{
+        wx.showToast({
+          title: res.info,
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  }
 })
